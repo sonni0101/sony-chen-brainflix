8d7821e511f31fa0810387014c68cf0ebe79a002
Sony Chen <hello@sony-chen.com>
2024-05-29 00:19:00 -0400

    Sprint one branch

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./logo.scss";
+
+function logo(){
+    return(
+        <>
+            <img scr="BrainFlix-logo.svg" alt="Logo images" />
+            <h1>test</h1>
+        </>
+    );
+}
+
+export default Logo;
