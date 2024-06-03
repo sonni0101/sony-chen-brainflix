cc4a32556ecf904ca1364dab09cfef30aac5c3cb
Sony Chen <hello@sony-chen.com>
2024-06-02 23:14:27 -0400

    rm css files

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import logo from './logo.svg';
-import './App.css';
 import './App.scss';
 import Header from './components/Header/Header.js';
 import VideoPlayer from './components/VideoPlayer/VideoPlayer.js';
