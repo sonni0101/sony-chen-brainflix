38a1cc19f42ac936684ed10de834d8bb81bc1182
Sony Chen <hello@sony-chen.com>
2024-06-23 00:30:17 -0600

    Added H1

--- src/pages/VideoUploadPage/VideoUploadPage.js
+++ src/pages/VideoUploadPage/VideoUploadPage.js
@@ -1,12 +1,15 @@
 import React from "react";
 import Header from "../../components/Header/Header";
 
+
 function VideoUploadPage(){
 
     return(
-        <>
+        <main>
             <Header />
-        </>
+            <h1>Upload Video</h1>
+
+        </main>
     );
 }
 
