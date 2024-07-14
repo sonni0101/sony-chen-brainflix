0d1124698799048f49ba4b3b06beb77d9d61958e
Sony Chen <hello@sony-chen.com>
2024-07-14 15:12:45 -0600

    Added API folders

---
+++ sony-chen-brainflix/src/components/DeleteButton/DeleteButton.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './DeleteButton.scss';
+
+function DeleteButton({onClick}){
+    return (
+        <>
+            <button className="delete-button" onClick={onClick}>Delete</button>
+        </>
+    );
+}
+
+export default DeleteButton;
